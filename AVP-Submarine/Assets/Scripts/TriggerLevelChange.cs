e06d06707f4c3108361b230b3cfc419cc2e29067
C <krzysztof2.chwiolka@live.uwe.ac.uk>
2020-02-11 16:08:04 +0100

    Scene Transition added

---
+++ AVP-Submarine/Assets/Scripts/TriggerLevelChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerLevelChange : MonoBehaviour {
+    public LevelTransition transition;
+
+    public bool goesToNextLevel = true;
+    public bool finishedLevel = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //other.GetComponent<PlayerControl>().FinishedLVL1(); //Player Controller - e.g. change relative position on spawn etc
+            finishedLevel = true;
+            transition.FadeToNextLevel(goesToNextLevel);
+        }
+    }
+}

6b7992beb1762d12fe77a5174d355659a4327e36
Courtney Daniels <Courtney2.daniels@live.uwe.ac.uk>
2020-04-19 20:26:43 +0100

    Added Text stuff and imported assets
    Gonna need a big old clean out of this project at some point. lots of redundant files in the assets i imported

---
+++ AVP-Submarine/Assets/Scripts/ClampText.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClampText : MonoBehaviour
+{
+    public Text infoBox;
+
+    void Update()
+    {
+        Vector3 boxPos = Camera.main.WorldToScreenPoint(this.transform.position);
+        infoBox.transform.position = boxPos;
+        
+    }
+}
